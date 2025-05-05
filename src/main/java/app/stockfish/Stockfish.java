2ec0443edd4f1fb1824767a55d8945f02e03ad69
Sebastian Wilt <wilt.sebastian@gmail.com>
2025-05-05 17:02:03 +0200

    Start integrating stockfish engine

---
+++ src/main/java/app/stockfish/Stockfish.java
@@ -0,0 +1,91 @@
+package app.stockfish;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.nio.file.Paths;
+
+public class Stockfish {
+    private String path;
+    private Process proc;
+    private BufferedReader input;
+    private BufferedWriter output;
+
+    public Stockfish(String path) {
+        this.path = path;
+        start();
+    }
+
+    private void start() {
+        var userDirectory = Paths.get("").toAbsolutePath().toString();
+        var pb = new ProcessBuilder(path).directory(new File(userDirectory));
+        pb.redirectOutput(ProcessBuilder.Redirect.PIPE);
+        pb.redirectInput(ProcessBuilder.Redirect.PIPE);
+        try {
+            proc = pb.start();
+            input = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            output = new BufferedWriter(new OutputStreamWriter(proc.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            output.write("uci\n");
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        try {
+            String msg;
+            do {
+                msg = input.readLine();
+                System.out.println(msg);
+            } while (!msg.equals("uciok"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public String getMove(String moves) {
+        try {
+            // Start stockfish 
+            output.write(String.format("position startpos moves %s\n", moves));
+            output.write("go\n");
+            output.flush();
+
+            // Give time to think
+            Thread.sleep(2000);
+            output.write("stop\n");
+            output.flush();
+
+            // Read move
+            String msg;
+            do {
+                msg = input.readLine();
+                System.out.println(msg);
+            } while (!msg.startsWith("bestmove"));
+
+            var parts = msg.split(" ");
+            System.out.format("Stockfish best move: %s\n", parts[1]);
+                
+            return parts[1];
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        return "";
+    }
+}
